2937ff59bac9c62cb2818c56793b2ba0e2295bd2
Chhavi <Chhavi.choudhary75@gmail.com>
2021-08-08 23:54:34 +0200

    lookups

--- object.js
+++ object.js
@@ -23,3 +23,32 @@ var dogs = {
   yourDog["bark"]="bho bhoo"
   yourDog.bark1="woof woof"
   console.log(yourDog)
+
+  var ourDog1 = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"],
+    "bark": "bow-wow"
+  };
+  delete ourDog1.legs;
+  console.log(ourDog1);
+
+
+
+
+  function phoneticLookup(val) {
+    var result = "";
+    var lookup= {
+      "alpha":"Adams",
+      "bravo":"Boston",
+      "charlie":"Chicago",
+      "delta":"Denver",
+      "echo":"Easy",
+     "foxtrot":"Frank"
+    }
+  return lookup[val]
+    // Only change code above this line
+    return result;
+  }
+  console.log(phoneticLookup("charlie"));
